76ef8e72d52814fd1709933bec544f15f80c61c5
woodr <jasper2banff@gmail.com>
2013-12-21 17:18:13 -0500

    Improvements

--- generator.py
+++ generator.py
@@ -1,29 +1,55 @@
-#!/python
-# woodr jun2013
+# generator.py woodr dec2013
+
+import sys
+import os
 
 from random import randint
 
-# Open output
-my_file = open('cloudnine.txt', 'w')
+class generator(object):
 
-# Generate one million sets
-for x in range(1000000):
-  # Get six random numbers
-  next_ticket = []
+    number_of_sets = 1000000
+    out_file = None
+    out_name = "cloudnine.txt"
 
-  for y in range(6):
-    n = randint(1,49)
-    # Can't have the same number twice in same set
-    while n in next_ticket:
-      n = randint(1,49)
-    # Add to list
-    next_ticket.append(n)
+    def __init__(self):
+        pass
 
-  # Sort ascending
-  next_ticket.sort()
-  # Write to file
-  my_file.write(str(next_ticket).strip('[]'))
-  my_file.write("\n")
+    def open_file(self):
+        self.out_file = open(self.out_name, 'w')
 
-# Close output
-my_file.close()
+    def write(self, text_out):
+        self.out_file.write(text_out)
+        self.out_file.write("\n")
+
+    def close_file(self):
+        self.out_file.close()
+
+    def generate_out(self):
+        # Generate sets
+        for x in range(1, self.number_of_sets + 1):        
+          # Get six random numbers
+          next_set = []
+          for y in range(6):
+            n = randint(1,49)
+            # Can't have the same number twice in same set
+            while n in next_set:
+              n = randint(1,49)
+            # Add to list
+            next_set.append(n)
+          # Sort ascending
+          next_set.sort()
+          # Write the number
+          self.write(str(next_set).strip('[]'))
+          print("Set %d: %s" %(x, next_set))
+
+
+def cli():
+    print("\n* Cloud Nine *\n")
+    gen = generator()
+    gen.open_file()
+    gen.generate_out()
+    gen.close_file()
+    print("\nFinished.\n")
+
+if __name__ == '__main__':
+    cli()
